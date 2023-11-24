3cf2fffe7e2ef3ac0363fea0625d99e4833ba29f
Hyunseok Ko <kohyunsuk98@gmail.com>
2023-11-25 00:13:51 +0900

    chore: 로컬 DB 설정

---
+++ lovebird-domain/build.gradle.kts
@@ -0,0 +1,33 @@
+description = "domain module"
+
+plugins {
+	idea
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+dependencies {
+	implementation(project(":lovebird-common"))
+
+	api("org.springframework.boot:spring-boot-starter-data-jpa")
+	api("com.querydsl:querydsl-jpa:${property("querydslVersion")}:jakarta")
+	kapt("com.querydsl:querydsl-apt:${property("querydslVersion")}:jakarta")
+	kapt("jakarta.annotation:jakarta.annotation-api")
+	kapt("jakarta.persistence:jakarta.persistence-api")
+
+	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("com.h2database:h2")
+}
+
+idea {
+	module {
+		val kaptMain = file("build/generated/source/kapt/main")
+		sourceDirs.add(kaptMain)
+		generatedSourceDirs.add(kaptMain)
+	}
+}
+
