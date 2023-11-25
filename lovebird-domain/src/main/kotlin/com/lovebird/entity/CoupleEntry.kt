8ebd05a4f67bd087663d0441b0450594f72a0f1d
Hyunseok Ko <kohyunsuk98@gmail.com>
2023-11-26 05:06:40 +0900

    feature: User 관련 엔티티 구성

---
+++ lovebird-domain/src/main/kotlin/com/lovebird/entity/CoupleEntry.kt
@@ -0,0 +1,33 @@
+package com.lovebird.entity
+
+import com.lovebird.entity.audit.AuditEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "couple_entry")
+class CoupleEntry(
+	user: User,
+	partner: User
+) : AuditEntity() {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "entry_id")
+	val id: Long? = null
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
+	val user: User = user
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "partner_id", referencedColumnName = "user_id")
+	val partner: User = partner
+}
