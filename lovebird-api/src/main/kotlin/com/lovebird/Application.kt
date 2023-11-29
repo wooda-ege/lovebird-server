787f9dbba1315a79b16d1caa685e2c96d3e8ee5d
Hyunseok Ko <kohyunsuk98@gmail.com>
2023-11-29 14:08:37 +0900

    chore: TimeZone 서울로 설정

--- lovebird-api/src/main/kotlin/com/lovebird/Application.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/Application.kt
@@ -1,11 +1,19 @@
 package com.lovebird
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class Application
 
+@PostConstruct
+fun started() {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+}
+
 fun main(args: Array<String>) {
+	started()
 	runApplication<Application>(*args)
 }
