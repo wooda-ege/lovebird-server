02449bb6293f5a4f36376a45fbc39e1d9649445a
Hyunseok Ko <kohyunsuk98@gmail.com>
2023-11-29 12:04:28 +0900

    feature: Jwt 관련 provider, validator, filter 구현

---
+++ lovebird-security/src/main/kotlin/com/lovebird/security/validator/JwtValidator.kt
@@ -0,0 +1,69 @@
+package com.lovebird.security.validator
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.lovebird.common.exception.LbException
+import com.lovebird.entity.User
+import com.lovebird.enums.ReturnCode
+import com.lovebird.repository.reader.UserReader
+import com.lovebird.security.vo.PrincipalUser
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.security.Key
+import java.security.PublicKey
+import java.util.Base64
+
+@Component
+class JwtValidator(
+	private val key: Key,
+	private val userReader: UserReader
+) {
+	fun getAuthentication(token: String): Authentication {
+		val claims = getTokenClaims(token)
+		val user: User = userReader.findEntityById(claims.get("id", String::class.java).toLong())
+		val principalUser: PrincipalUser = PrincipalUser.of(user)
+
+		return UsernamePasswordAuthenticationToken(principalUser, "", principalUser.authorities)
+	}
+
+	@Suppress("UNCHECKED_CAST")
+	fun parseHeaders(token: String): Map<String, String> {
+		val header = token.split(".")[0]
+
+		return ObjectMapper().readValue(
+			decodeHeader(header),
+			MutableMap::class.java
+		) as Map<String, String>
+	}
+
+	fun decodeHeader(token: String): String {
+		return String(Base64.getDecoder().decode(token), StandardCharsets.UTF_8)
+	}
+
+	fun getTokenClaims(token: String): Claims {
+		try {
+			return Jwts.parserBuilder()
+				.setSigningKey(key)
+				.build()
+				.parseClaimsJws(token)
+				.body
+		} catch (e: RuntimeException) {
+			throw LbException(ReturnCode.WRONG_JWT_TOKEN)
+		}
+	}
+
+	fun getTokenClaims(token: String, publicKey: PublicKey): Claims {
+		try {
+			return Jwts.parserBuilder()
+				.setSigningKey(publicKey)
+				.build()
+				.parseClaimsJws(token)
+				.body
+		} catch (e: RuntimeException) {
+			throw LbException(ReturnCode.WRONG_JWT_TOKEN)
+		}
+	}
+}
