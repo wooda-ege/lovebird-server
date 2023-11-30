767d89b9dd52ca977fadd95a5cc0a32bc6b3c9c3
Hyunseok Ko <kohyunsuk98@gmail.com>
2023-11-30 09:31:17 +0900

    docs: 어노테이션 domcument 작성

--- lovebird-common/src/main/kotlin/com/lovebird/annotation/Reader.kt
+++ lovebird-common/src/main/kotlin/com/lovebird/annotation/Reader.kt
@@ -1,9 +1,28 @@
 package com.lovebird.annotation
 
+import org.springframework.core.annotation.AliasFor
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
+
+/**
+ * Indicates that an annotated class is a "Reader" (e.g. a data access object).
+ *
+ * <p>This annotation serves as a specialization of {@link Component @Component},
+ * allowing for implementation classes to be autodetected through classpath scanning.
+ *
+ * @author komment
+ * @see Component
+ * @see Repository
+ */
 
 @Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 @Component
-annotation class Reader(val value: String = "")
+annotation class Reader(
+	/**
+	 * Alias for {@link Component#value}.
+	 */
+	@get:AliasFor(annotation = Component::class)
+	val value: String = ""
+)
