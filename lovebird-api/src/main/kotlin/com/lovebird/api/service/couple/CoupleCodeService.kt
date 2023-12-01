9000d71103cabc837c1673618e70dc2bfd159a3c
Hyunseok Ko <kohyunsuk98@gmail.com>
2023-12-02 01:10:17 +0900

    feature: 커플 관련 API 개발

---
+++ lovebird-api/src/main/kotlin/com/lovebird/api/service/couple/CoupleCodeService.kt
@@ -0,0 +1,74 @@
+package com.lovebird.api.service.couple
+
+import com.lovebird.api.dto.param.couple.CoupleLinkParam
+import com.lovebird.api.dto.response.couple.CoupleCodeResponse
+import com.lovebird.api.dto.response.couple.CoupleLinkResponse
+import com.lovebird.common.enums.ReturnCode
+import com.lovebird.common.exception.LbException
+import com.lovebird.domain.entity.CoupleCode
+import com.lovebird.domain.entity.User
+import com.lovebird.domain.repository.reader.CoupleCodeReader
+import com.lovebird.domain.repository.writer.CoupleCodeWriter
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class CoupleCodeService(
+	private val coupleService: CoupleService,
+	private val coupleCodeWriter: CoupleCodeWriter,
+	private val coupleCodeReader: CoupleCodeReader
+) {
+
+	private val ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz"
+
+	@Transactional
+	fun generateCodeIfNotExist(user: User): CoupleCodeResponse {
+		val coupleCode = findByUser(user) ?: save(user, generateCode())
+
+		return CoupleCodeResponse(coupleCode.code, coupleCode.getRemainSeconds())
+	}
+
+	@Transactional
+	fun linkCouple(param: CoupleLinkParam): CoupleLinkResponse {
+		val coupleCode = findByUser(param.user) ?: throw LbException(ReturnCode.WRONG_PARAMETER)
+		coupleService.saveAll(param.user, coupleCode.user)
+
+		return CoupleLinkResponse(coupleCode.id!!)
+	}
+
+	private fun save(user: User, code: String): CoupleCode {
+		return coupleCodeWriter.save(CoupleCode(user, code))
+	}
+
+	private fun delete(coupleCode: CoupleCode) {
+		coupleCodeWriter.delete(coupleCode)
+	}
+
+	private fun generateCode(): String {
+		var builder: StringBuilder
+
+		do {
+			builder = StringBuilder(8)
+
+			for (i in 0..7) {
+				val character = (Math.random() * ALPHA_NUMERIC_STRING.length).toInt()
+				builder.append(ALPHA_NUMERIC_STRING[character])
+			}
+		} while (!existsByCode(builder.toString()))
+
+		return builder.toString()
+	}
+
+	private fun findByUser(user: User): CoupleCode? {
+		return coupleCodeReader.findByUser(user)?.let {
+			if (it.isExpired()) {
+				delete(it)
+				null
+			} else it
+		}
+	}
+
+	private fun existsByCode(code: String): Boolean {
+		return coupleCodeReader.existsByCode(code)
+	}
+}
--- lovebird-api/src/main/kotlin/com/lovebird/api/service/couple/CoupleService.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/service/couple/CoupleService.kt
@@ -1,15 +1,27 @@
 package com.lovebird.api.service.couple
 
+import com.lovebird.domain.entity.CoupleEntry
 import com.lovebird.domain.entity.User
-import com.lovebird.domain.repository.reader.CoupleReader
+import com.lovebird.domain.repository.reader.CoupleEntryReader
+import com.lovebird.domain.repository.writer.CoupleEntryWriter
 import org.springframework.stereotype.Service
 
 @Service
 class CoupleService(
-	private val coupleReader: CoupleReader
+	private val coupleEntryReader: CoupleEntryReader,
+	private val coupleEntryWriter: CoupleEntryWriter
 ) {
 
+	fun saveAll(user1: User, user2: User) {
+		coupleEntryWriter.saveAll(
+			listOf(
+				CoupleEntry(user1, user2),
+				CoupleEntry(user2, user1)
+			)
+		)
+	}
+
 	fun existByUser(user: User): Boolean {
-		return coupleReader.existsByUser(user)
+		return coupleEntryReader.existsByUser(user)
 	}
 }
