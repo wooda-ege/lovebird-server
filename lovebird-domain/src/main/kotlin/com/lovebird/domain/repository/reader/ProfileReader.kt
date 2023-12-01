897ee2b7cafe378eff20e2ec606fc044b100db17
Hyunseok Ko <kohyunsuk98@gmail.com>
2023-12-01 15:46:52 +0900

    feature: Profile 상세 조회, 수정 관련 쿼리 작성

--- lovebird-domain/src/main/kotlin/com/lovebird/domain/repository/reader/ProfileReader.kt
+++ lovebird-domain/src/main/kotlin/com/lovebird/domain/repository/reader/ProfileReader.kt
@@ -1,17 +1,26 @@
 package com.lovebird.domain.repository.reader
 
+import com.lovebird.common.enums.ReturnCode
+import com.lovebird.common.exception.LbException
+import com.lovebird.domain.dto.query.ProfileDetailParam
 import com.lovebird.domain.entity.Profile
 import com.lovebird.domain.entity.User
 import com.lovebird.domain.repository.jpa.ProfileJpaRepository
+import com.lovebird.domain.repository.query.ProfileQueryRepository
 import jakarta.persistence.EntityNotFoundException
 import org.springframework.stereotype.Component
 
 @Component
 class ProfileReader(
-	private val profileJpaRepository: ProfileJpaRepository
+	private val profileJpaRepository: ProfileJpaRepository,
+	private val profileQueryRepository: ProfileQueryRepository
 ) {
 
 	fun findEntityByUser(user: User): Profile {
 		return profileJpaRepository.findByUser(user) ?: throw EntityNotFoundException()
+	}
+
+	fun findDetailParamByUser(user: User): ProfileDetailParam {
+		return profileQueryRepository.findDetailParamByUser(user) ?: throw LbException(ReturnCode.NOT_EXIST_PROFILE)
 	}
 }
--- lovebird-domain/src/main/kotlin/com/lovebird/domain/repository/writer/ProfileWriter.kt
+++ lovebird-domain/src/main/kotlin/com/lovebird/domain/repository/writer/ProfileWriter.kt
@@ -1,5 +1,6 @@
 package com.lovebird.domain.repository.writer
 
+import com.lovebird.domain.dto.command.ProfileUpdateParam
 import com.lovebird.domain.entity.Profile
 import com.lovebird.domain.repository.jpa.ProfileJpaRepository
 import org.springframework.stereotype.Component
@@ -11,5 +12,9 @@ class ProfileWriter(
 
 	fun save(profile: Profile) {
 		profileJpaRepository.save(profile)
+	}
+
+	fun update(profile: Profile, param: ProfileUpdateParam) {
+		profile.update(param)
 	}
 }
