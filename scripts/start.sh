46b90803a1513c3c84786aeef978feec2d091ed5
Hyunseok Ko <kohyunsuk98@gmail.com>
2023-12-12 18:36:20 +0900

    chore: docker 컨테이너 timezone 설정

--- scripts/start.sh
+++ scripts/start.sh
@@ -18,8 +18,8 @@ cd $PROJECT_ROOT
 
 echo "$TIME_NOW > docker build --platform linux/amd64 --build-arg DEPENDENCY=build/dependency -t $IMAGE_NAME ." >> $DEPLOY_LOG
 docker build --platform linux/amd64 --build-arg DEPENDENCY=build/dependency -t $IMAGE_NAME .
-echo "$TIME_NOW > docker run --restart always --name $CONTAINER_NAME -d -p 8080:8080 $IMAGE_NAME" >> $DEPLOY_LOG
-docker run --restart always --name $CONTAINER_NAME -d -p 8080:8080 $IMAGE_NAME
+echo "$TIME_NOW > docker run --restart always -e TZ=Asia/Seoul --name $CONTAINER_NAME -d -p 8080:8080 $IMAGE_NAME" >> $DEPLOY_LOG
+docker run --restart always -e TZ=Asia/Seoul --name $CONTAINER_NAME -d -p 8080:8080 $IMAGE_NAME
 
 CURRENT_CONTAINER_ID=$(docker container ls -q)
 echo "$TIME_NOW > 실행된 컨테이너 아이디 $CURRENT_CONTAINER_ID 입니다." >> $DEPLOY_LOG
