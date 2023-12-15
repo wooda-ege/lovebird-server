5b078c673f3c53787eba3777e22b9ba19a45d570
Yong <amico741@naver.com>
2023-12-15 15:39:30 +0900

    fix: Exception 발생 사유 클라이언트 단으로 변경

--- lovebird-domain/src/main/kotlin/com/lovebird/domain/repository/reader/CalendarReader.kt
+++ lovebird-domain/src/main/kotlin/com/lovebird/domain/repository/reader/CalendarReader.kt
@@ -1,9 +1,10 @@
 package com.lovebird.domain.repository.reader
 
+import com.lovebird.common.enums.ReturnCode
+import com.lovebird.common.exception.LbException
 import com.lovebird.domain.annotation.Reader
 import com.lovebird.domain.entity.Calendar
 import com.lovebird.domain.repository.jpa.CalendarJpaRepository
-import jakarta.persistence.EntityNotFoundException
 
 @Reader
 class CalendarReader(
@@ -11,6 +12,6 @@ class CalendarReader(
 ) {
 
 	fun findEntityById(id: Long): Calendar {
-		return calendarJpaRepository.findById(id).orElseThrow() { throw EntityNotFoundException() }
+		return calendarJpaRepository.findById(id).orElseThrow() { throw LbException(ReturnCode.WRONG_PARAMETER) }
 	}
 }
