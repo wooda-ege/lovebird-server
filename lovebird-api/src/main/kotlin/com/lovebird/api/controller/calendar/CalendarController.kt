804e4ed70530e690534094bc4be637bf37daa24c
Yong <amico741@naver.com>
2023-12-15 15:39:45 +0900

    fix: Resource 복수로 변경

--- lovebird-api/src/main/kotlin/com/lovebird/api/controller/calendar/CalendarController.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/controller/calendar/CalendarController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/calendar")
+@RequestMapping("/api/v1/calendars")
 class CalendarController(
 	private val calendarService: CalendarService
 ) {
