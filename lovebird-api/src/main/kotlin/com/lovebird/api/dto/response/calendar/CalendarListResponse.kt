a7508fda0080cc60e1ae728646da16c240fd9993
Yong <amico741@naver.com>
2023-12-16 17:46:42 +0900

    fix: List의 totalCount response에 반영하도록 수정

--- lovebird-api/src/main/kotlin/com/lovebird/api/dto/response/calendar/CalendarListResponse.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/dto/response/calendar/CalendarListResponse.kt
@@ -3,11 +3,12 @@ package com.lovebird.api.dto.response.calendar
 import com.lovebird.domain.dto.query.CalendarListResponseParam
 
 data class CalendarListResponse(
-	val calendars: List<CalendarListResponseParam>
+	val calendars: List<CalendarListResponseParam>,
+	val totalCount: Int
 ) {
 	companion object {
 		fun of(calendars: List<CalendarListResponseParam>): CalendarListResponse {
-			return CalendarListResponse(calendars)
+			return CalendarListResponse(calendars, calendars.size)
 		}
 	}
 }
