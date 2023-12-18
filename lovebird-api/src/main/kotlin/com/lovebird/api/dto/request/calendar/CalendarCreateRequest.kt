f23d2285782929645d03bc898be9db71f70554ac
Yong <amico741@naver.com>
2023-12-18 16:57:18 +0900

    fix: Nullable 관련 애노테이션 제거

--- lovebird-api/src/main/kotlin/com/lovebird/api/dto/request/calendar/CalendarCreateRequest.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/dto/request/calendar/CalendarCreateRequest.kt
@@ -4,20 +4,15 @@ import com.lovebird.common.enums.Alarm
 import com.lovebird.common.enums.Color
 import com.lovebird.domain.entity.Calendar
 import com.lovebird.domain.entity.User
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.NotNull
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDate
 import java.time.LocalTime
 
 data class CalendarCreateRequest(
-	@NotBlank
 	val title: String,
 	val memo: String?,
-	@NotNull
 	val color: Color,
 	val alarm: Alarm?,
-	@NotNull
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
 	val startDate: LocalDate,
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
