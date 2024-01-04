c2d5fff184f079091ef2c909085519cd7ebc8c34
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-01-04 15:42:07 +0900

    refactor: security 모듈 제거

--- lovebird-security/src/main/kotlin/com/lovebird/security/annotation/AuthorizedUser.kt
+++
@@ -1,5 +0,0 @@
-package com.lovebird.security.annotation
-
-@Target(AnnotationTarget.VALUE_PARAMETER)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class AuthorizedUser()
--- lovebird-security/src/main/kotlin/com/lovebird/security/dto/param/NaverLoginParam.kt
+++
@@ -1,6 +0,0 @@
-package com.lovebird.security.dto.param
-
-data class NaverLoginParam(
-	val code: String,
-	val state: String
-)
