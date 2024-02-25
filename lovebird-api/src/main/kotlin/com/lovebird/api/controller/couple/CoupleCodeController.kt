641e5b31ba219f5ce926af8adc246ed93e2b42f7
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-02-25 16:25:25 +0900

    fix: Http Method 수정

--- lovebird-api/src/main/kotlin/com/lovebird/api/controller/couple/CoupleCodeController.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/controller/couple/CoupleCodeController.kt
@@ -7,6 +7,7 @@ import com.lovebird.api.dto.response.couple.CoupleLinkResponse
 import com.lovebird.api.service.couple.CoupleCodeService
 import com.lovebird.common.response.ApiResponse
 import com.lovebird.domain.entity.User
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,7 +19,7 @@ class CoupleCodeController(
 	private val coupleCodeService: CoupleCodeService
 ) {
 
-	@PostMapping("/code")
+	@GetMapping("/code")
 	fun issueCoupleCode(@AuthorizedUser user: User): ApiResponse<CoupleCodeResponse> {
 		return ApiResponse.success(coupleCodeService.generateCodeIfNotExist(user))
 	}
