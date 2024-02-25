70e417802cf1256292f6effde92f31f192064427
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-02-25 15:14:57 +0900

    refactor: import 와일드 카드 제거

--- lovebird-api/src/main/kotlin/com/lovebird/api/provider/JwtProvider.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/provider/JwtProvider.kt
@@ -9,7 +9,7 @@ import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.security.Key
-import java.util.*
+import java.util.Date
 
 @Component
 class JwtProvider(
