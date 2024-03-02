e1fe4e5e75f2f7dca0c55e6a792995cb3f2ac661
Yong <amico741@naver.com>
2024-03-02 18:24:33 +0900

    feature: 커플 도메인 전체 삭제 로직 추가

--- lovebird-domain/src/main/kotlin/com/lovebird/domain/repository/jpa/CoupleEntryJpaRepository.kt
+++ lovebird-domain/src/main/kotlin/com/lovebird/domain/repository/jpa/CoupleEntryJpaRepository.kt
@@ -1,8 +1,14 @@
 package com.lovebird.domain.repository.jpa
 
 import com.lovebird.domain.entity.CoupleEntry
+import com.lovebird.domain.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CoupleEntryJpaRepository : JpaRepository<CoupleEntry, Long>
+interface CoupleEntryJpaRepository : JpaRepository<CoupleEntry, Long> {
+
+	fun deleteByPartner(partner: User)
+
+	fun deleteByUser(user: User)
+}
