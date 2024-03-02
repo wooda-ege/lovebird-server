c53d28a1a266c1e28881e81c3da7b1cfe86ee513
Yong <amico741@naver.com>
2024-03-02 18:21:26 +0900

    feature: 삭제 로직을 위한 퍼사드 패턴 적용하기 위한 클래스 추가

---
+++ lovebird-api/src/main/kotlin/com/lovebird/api/service/user/AuthProcessingService.kt
@@ -0,0 +1,28 @@
+package com.lovebird.api.service.user
+
+import com.lovebird.api.service.calendar.CalendarService
+import com.lovebird.api.service.couple.CoupleService
+import com.lovebird.api.service.diary.DiaryService
+import com.lovebird.api.service.profile.ProfileService
+import com.lovebird.domain.entity.User
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class AuthProcessingService(
+	private val userService: UserService,
+	private val diaryService: DiaryService,
+	private val calendarService: CalendarService,
+	private val coupleService: CoupleService,
+	private val profileService: ProfileService
+) {
+
+	@Transactional
+	fun deleteAccount(user: User) {
+		diaryService.delete(user)
+		calendarService.delete(user)
+		coupleService.delete(user)
+		profileService.delete(user)
+		userService.delete(user)
+	}
+}
