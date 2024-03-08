8cae1740c7b2ee78849ca67beecb9eb0c268550c
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-03-08 11:11:54 +0900

    fix: s3 경로 관련 dto 수정

--- lovebird-api/src/main/kotlin/com/lovebird/api/dto/param/external/DiaryUploadPresignedUrlParam.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/dto/param/external/DiaryUploadPresignedUrlParam.kt
@@ -2,6 +2,5 @@ package com.lovebird.api.dto.param.external
 
 data class DiaryUploadPresignedUrlParam(
 	val userId: Long,
-	val diaryId: Long,
 	val filenames: List<String>
 )
--- lovebird-api/src/main/kotlin/com/lovebird/api/dto/request/external/DiaryUploadPresignedUrlRequest.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/dto/request/external/DiaryUploadPresignedUrlRequest.kt
@@ -4,9 +4,8 @@ import com.lovebird.api.dto.param.external.DiaryUploadPresignedUrlParam
 
 data class DiaryUploadPresignedUrlRequest(
 	val filenames: List<String>,
-	val diaryId: Long
 ) {
 	fun toParam(userId: Long): DiaryUploadPresignedUrlParam {
-		return DiaryUploadPresignedUrlParam(userId, diaryId, filenames)
+		return DiaryUploadPresignedUrlParam(userId, filenames)
 	}
 }
