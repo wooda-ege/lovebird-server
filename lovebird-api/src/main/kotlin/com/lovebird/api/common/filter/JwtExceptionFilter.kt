34ddd3cf89eca74919960db96b93473990342c7b
TAEYONG KIM <86272688+YongsHub@users.noreply.github.com>
2024-03-31 15:57:29 +0900

    Hotfix | #81 | @YongsHub | Exception FIlter에 에러 로그 추가

--- lovebird-api/src/main/kotlin/com/lovebird/api/common/filter/JwtExceptionFilter.kt
+++ lovebird-api/src/main/kotlin/com/lovebird/api/common/filter/JwtExceptionFilter.kt
@@ -7,17 +7,20 @@ import com.lovebird.common.response.ApiResponse
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 
 @Component
 class JwtExceptionFilter : OncePerRequestFilter() {
+	private val log = LoggerFactory.getLogger(javaClass)
 
 	override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
 		try {
 			filterChain.doFilter(request, response)
 		} catch (e: LbException) {
+			log.error(e.getMsg())
 			setErrorResponse(e.getReturnCode(), response)
 		}
 	}
